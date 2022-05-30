3574318f44057ba3d6599c97844b16596f767f69
Johnny Urdin <urdin32@gmail.com>
2022-05-30 10:31:48 -0500

    Cambios en la plantilla

--- nodusnet/settings.py
+++ nodusnet/settings.py
@@ -123,6 +123,11 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS=[
      'static',
 ]
+
+MEDIA_URL="media"
+
+MEDIA_ROOT="/mnt/7C4582971554AF5A/var/www/nodusnet/media"
+
 #STATIC_ROOT="/mnt/7C4582971554AF5A/var/www/nodusnet/static"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
