14e5928188077f477865a4bbdb4158971a354615
Johnny Urdin <urdin32@gmail.com>
2022-05-30 10:40:42 -0500

    Cambios en la plantilla

--- Inicio/views.py
+++ Inicio/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+from Inicio.models import Configuracion
+
+
 def index(request):
-    return render(request,"index-corporate.html")
+    contexto={
+        'configuracion':Configuracion.objects.last()
+    }
+    return render(request,"index-corporate.html",contexto)
