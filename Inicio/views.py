0a6645efddf2cff6cbcb1cf54a8e65d420666ea2
Johnny Urdin <urdin32@gmail.com>
2022-05-30 14:39:14 -0500

    Cambios en la plantilla

--- Inicio/views.py
+++ Inicio/views.py
@@ -3,10 +3,30 @@ from django.shortcuts import render
 # Create your views here.
 from Inicio.models import Configuracion, EventosProximos
 
+def llenado_informacion(request):
+    configuracion = Configuracion.objects.last()
+    print(configuracion)
+    if configuracion:
+        if configuracion.logo:
+            request.session["logo"] = "/media/%s" % str(configuracion.logo)
+        if configuracion.telefono:
+            request.session["telefono"] = configuracion.telefono
+        if configuracion.facebook:
+            request.session["facebook"] = configuracion.facebook
+        if configuracion.email:
+            request.session["email"] = configuracion.email
+        if configuracion.numero_clientes:
+            request.session["numero_clientes"] = configuracion.numero_clientes
+        if configuracion.numero_corporativos:
+            request.session["numero_corporativos"] = configuracion.numero_corporativos
+
 
 def index(request):
+    configuracion=Configuracion.objects.last()
+    llenado_informacion(request)
+
     contexto={
-        'configuracion':Configuracion.objects.last(),
+        'configuracion':configuracion,
         'eventos':EventosProximos.objects.all(),
     }
     return render(request,"index-corporate.html",contexto)
