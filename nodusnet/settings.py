5d36aa17d959cf85842edc04989e2714ad270a45
Johnny Urdin <urdin-23@live.com>
2022-05-31 21:23:18 -0500

    Plantilla de Blog

--- nodusnet/settings.py
+++ nodusnet/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2=6lqhiprqk*6a@)@f%9cih_p0#*a21$hp-n$nw_wsroak9wtg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
