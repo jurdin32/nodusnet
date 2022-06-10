9b7670f2a300b74f2dbfeb14116b22388462404f
Johnny Urdin <urdin32@gmail.com>
2022-06-10 16:21:55 -0500

    Cambios en la plantilla

--- Inicio/views.py
+++ Inicio/views.py
@@ -11,7 +11,7 @@ def index(request):
     contexto={
         'sliders':sliders,
         'configuracion':configuracion,
-        'eventos':EventosProximos.objects.all(),
+        'eventos':EventosProximos.objects.all().order_by('-id'),
     }
     return render(request,"index-corporate.html",contexto)
 
